fb7d757cbe1ecab5578bec26fb17b1fec4c995d6
Hyung Ju Moon <hyungjumoon@gmail.com>
2020-05-22 15:33:44 +0900

    exercise1.1 again

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,24 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.part} {props.exercise}</p>
-    </div>
-  )
-}
-
-const Content2 = (props) => {
-  return (
-    <div>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
-    </div>
-  )
-}
-
 const Content = (props) => {
   return (
     <div>
